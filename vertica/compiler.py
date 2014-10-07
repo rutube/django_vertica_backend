b655b4df33880b0da1120b541b95c62195fc1082
bourivouh <bourivouh@gmail.com>
2014-10-07 14:08:25 +0400

    add enforce constraints validation flag

--- vertica/compiler.py
+++ vertica/compiler.py
@@ -1,23 +1,23 @@
 # coding: utf-8
 
+from django.conf import settings
 from django.db.models.sql.compiler import *
+
+# Vertica doesn't validate constraints when data are loaded into a table.
+# Constraints violation will cause errors at run time.
+# This flag enforces constraints validation after data inserting.
+# Maybe, it's usefull to validate constraints after updating or deleting, too?
+ENFORCE_CONSTRAINTS_VALIDATION = getattr(settings, "ENFORCE_CONSTRAINTS_VALIDATION", True)
 
 
 class SQLInsertCompiler(SQLInsertCompiler):
 
     def execute_sql(self, return_id=False):
-        assert not (return_id and len(self.query.objs) != 1)
-        self.return_id = return_id
-        cursor = self.connection.cursor()
-        for sql, params in self.as_sql():
-            cursor.execute(sql, params)
+        result = super(SQLInsertCompiler, self).execute_sql(return_id)
 
-        self.connection.ops.validate_constraints(
-            cursor, self.query.get_meta().db_table)
+        if ENFORCE_CONSTRAINTS_VALIDATION:
+            self.connection.ops.validate_constraints(
+                self.connection.cursor(),
+                self.query.get_meta().db_table)
 
-        if not (return_id and cursor):
-            return
-        if self.connection.features.can_return_id_from_insert:
-            return self.connection.ops.fetch_returned_insert_id(cursor)
-        return self.connection.ops.last_insert_id(cursor,
-                self.query.get_meta().db_table, self.query.get_meta().pk.column)
+        return result
